07103233ae9290f09101a60e28277cf8ca2cbeb0
Thamires G <146814172+Thamycurso@users.noreply.github.com>
2025-11-30 16:56:56 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Bot online como ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong! 🏓');
+  }
+});
+
+client.login(process.env.TOKEN);
